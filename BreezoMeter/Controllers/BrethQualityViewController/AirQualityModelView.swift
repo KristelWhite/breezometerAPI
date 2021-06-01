3bfe5f0a83dd5ee347455dd7119c03f7a60172d7
KristelWhite <KristelWhite@users.noreply.github.com>
2021-06-01 16:04:31 +0300

    save under realize rfp

---
+++ BreezoMeter/Controllers/BrethQualityViewController/AirQualityModelView.swift
@@ -0,0 +1,16 @@
+//
+//  AirQualityModelView.swift
+//  BreezoMeter
+//
+//  Created by Кристина Пастухова on 01.06.2021.
+//  Copyright © 2021 Кристина Пастухова. All rights reserved.
+//
+
+import Foundation
+
+
+class AirQualityModelView {
+    
+    
+    
+}
